a661dd66b972b278e1b769b11b49fa65eded8a5a
shahriar100 <shahriar.hasan100@gmail.com>
2021-01-26 23:47:22 +0600

    Test case initialization

--- backend/todos/tests.py
+++ backend/todos/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Todo
+
+# Create your tests here
+
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title="first todo", body="a body here")
