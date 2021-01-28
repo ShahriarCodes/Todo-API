914b3a7b5d8cef7a4502152d85e586157cb0ab79
shahriar100 <shahriar.hasan100@gmail.com>
2021-01-28 23:52:01 +0600

    Test cases

--- backend/todos/tests.py
+++ backend/todos/tests.py
@@ -9,3 +9,12 @@ class TodoModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         Todo.objects.create(title="first todo", body="a body here")
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.title}'
+        self.assertEquals(expected_object_name, 'first todo')
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.body}'
+        self.assertEquals(expected_object_name, 'a body here')
